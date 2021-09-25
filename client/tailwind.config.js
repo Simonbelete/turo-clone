269f12f2bc417fec5ee8fdc1cdab02214db83956
Simonbelete <simonbelete@gmail.com>
2021-09-25 12:52:38 +0300

    fix: 🐛 tailwindcss config
    changing purge dir to components

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
    // These paths are just examples, customize them to match your project structure
    purge: [
      './public/**/*.html',
-     './src/**/*.{js,jsx,ts,tsx,vue}',
+     './components/**/*.{js,jsx,ts,tsx,vue}',
    ],
   darkMode: false, // or 'media' or 'class'
   theme: {
