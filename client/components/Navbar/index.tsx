5ee41d5ba6fac6b7f97cbeffa856cd855aaef7a1
Simonbelete <simonbelete@gmail.com>
2021-10-11 15:24:19 +0300

    feat: 🎸 client removing atomic design

---
+++ client/components/Navbar/index.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react';
+
+const Navbar = (): ReactElement => {
+  return <div>{/* Logo */}</div>;
+};
+
+export default Navbar;
--- client/components/atoms/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Input } from './Input';
-
-export default null;
