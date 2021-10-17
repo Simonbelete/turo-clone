7eb6b483a7bb96282763df51b042b24911c8cfd8
Simonbelete <simonbelete@gmail.com>
2021-10-17 16:02:26 +0300

    client - banner responsive font

--- client/components/Banner/Banner.tsx
+++ client/components/Banner/Banner.tsx
@@ -10,7 +10,7 @@ interface BannerProps {
 const Banner = ({ children }: BannerProps): ReactElement => {
   return (
     <div className="w-full text-[#414143] bg-[#f4f4f4]">
-      <div className="flex flex-row font-basis-grotesque font-medium text-base justify-center items-center py-3 underline">
+      <div className="flex flex-row font-basis-grotesque font-medium text-xs md:text-base justify-center items-center py-3 underline">
         {children}
       </div>
     </div>
