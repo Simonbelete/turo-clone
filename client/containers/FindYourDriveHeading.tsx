e76fc7cdcaf8b9a08b3fd93723d832992fde49e8
Simonbelete <simonbelete@gmail.com>
2021-11-09 00:25:32 +0300

    client find your driving heading container

---
+++ client/containers/FindYourDriveHeading.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+
+import { Heading } from 'components';
+
+const FindYourDriveHeadingContainer = (): ReactElement => {
+  return (
+    <div>
+      <Heading className="font-freight font-black">Find your drive</Heading>
+    </div>
+  );
+};
+
+export default FindYourDriveHeadingContainer;
--- client/containers/index.ts
+++ client/containers/index.ts
@@ -1,4 +1,5 @@
 export { default as COVIDBanner } from './COVIDBanner';
 export { default as Hero } from './Hero';
+export { default as FindYourDriveHeading } from './FindYourDriveHeading';
 
 export default null;
