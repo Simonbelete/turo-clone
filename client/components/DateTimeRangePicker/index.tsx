ed01b1c0e26d01445903884cbded42a4bc6be3f1
Simonbelete <simonbelete@gmail.com>
2021-11-13 22:01:38 +0300

    day nfs

--- client/components/DateTimeRangePicker/index.tsx
+++ client/components/DateTimeRangePicker/index.tsx
@@ -1,19 +1,37 @@
 import React, { ReactElement, useState } from 'react';
-import { DateRangePicker } from 'react-dates';
-import moment, { Moment } from 'moment';
+import { DateRangePicker, START_DATE, END_DATE } from 'react-nice-dates';
+import { enGB } from 'date-fns/locale';
 
 const DateTimeRangePicker = (): ReactElement => {
-  const [date, setDate] = useState<Moment[] | null[]>([moment(), moment()]);
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
   return (
-    <div className="flex flex-row">
-      <DateRangePicker
-        startDate={null}
-        endDate={null}
-        // onDatesChange={({ startDate, endDate }) =>
-        //   setDate([])
-        // }
-      />
-    </div>
+    <DateRangePicker
+      startDate={startDate as Date}
+      endDate={endDate as Date}
+      onStartDateChange={setStartDate}
+      onEndDateChange={setEndDate}
+      minimumDate={new Date()}
+      minimumLength={1}
+      format="dd MMM yyyy"
+      locale={enGB}
+    >
+      {({ startDateInputProps, endDateInputProps, focus }) => (
+        <div className="date-range">
+          <input
+            className={`input${focus === START_DATE ? ' -focused' : ''}`}
+            {...startDateInputProps}
+            placeholder="Start date"
+          />
+          {/* <span className="date-range_arrow" /> */}
+          <input
+            className={`input${focus === END_DATE ? ' -focused' : ''}`}
+            {...endDateInputProps}
+            placeholder="End date"
+          />
+        </div>
+      )}
+    </DateRangePicker>
   );
 };
 
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation';
 import 'antd/dist/antd.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-dates/initialize';
+import 'react-nice-dates/build/style.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
